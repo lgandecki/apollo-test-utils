7b1b53b7065c6b9e6182a94709dc2c950c4fc972
Łukasz Gandecki <lgandecki@css.edu>
2017-12-25 10:28:30 +0400

    expose makeExecutableSchema to avoid the graphql-tools collision

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './util/isApolloClient';
 export * from './mocks/mockNetworkInterface';
 export * from './mocks/mockNetworkInterfaceWithSchema';
+// We export the graphql-tools used by test-utils to avoid the
+// "Also ensure that there are not multiple versions of GraphQL installed in your node_modules directory." error.
+// In particular, makeExecutableSchema should be used from this export instead of graphql-tools used directly by your project
+export * from 'graphql-tools';
+
